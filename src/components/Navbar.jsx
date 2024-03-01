edbad6ffe6f709ad8bb490c68539c20c937af694
felixNyalenda <felixasejunior@gmail.com>
2024-03-01 12:34:26 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { logo } from "../assets";
+
+function Navbar() {
+  return (
+    <header className="border-b h-20">
+      <div className="mx-auto w-full h-full max-w-7xl flex-between">
+        {/* logo */}
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-32 object-contain" />
+        </Link>
+
+        <nav className="flex-between gap-8 pr-8 text-base font-semibold">
+          <Link to="/" className="text-primary underline">
+            Home
+          </Link>
+          <Link to="/" className="text-primary">
+            Final Results
+          </Link>
+          <Link to="/" className="text-primary">
+            Final Voter Register
+          </Link>
+          <Link to="/" className="text-primary">
+            FKF Constitution
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import { Footer, Navbar } from "../components";
+
+function Layout() {
+  return (
+    <section className="w-screen">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </section>
+  );
+}
+
+export default Layout;
